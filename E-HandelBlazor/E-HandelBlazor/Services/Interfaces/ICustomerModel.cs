6b28c91299c670744002c08f40054ee5ff805f1d
lear23 <130910243+lear23@users.noreply.github.com>
2024-07-03 08:20:02 +0200

    löste problem med webAssembly

---
+++ E-HandelBlazor/E-HandelBlazor/Services/Interfaces/ICustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using E_Handel.Dtos;
+
+namespace E_HandelBlazor.Services.Interfaces;
+
+public interface ICustomerModel
+{
+    Task<ResponseDto<List<CustomerDto>>> List( string rol, string search);
+    Task<ResponseDto<CustomerDto>> Get(int id);
+    Task<ResponseDto<SessionDto>> Auth(LoginDto model);
+    Task<ResponseDto<CustomerDto>> Create(CustomerDto model);
+    Task<ResponseDto<bool>> Update(CustomerDto model);
+    Task<ResponseDto<bool>> Delete(int id);
+
+}
---
+++ E-HandelBlazor/E-HandelBlazor/Services/Models/CustomerModel.cs
@@ -0,0 +1,42 @@
+﻿using E_Handel.Dtos;
+using E_HandelBlazor.Services.Interfaces;
+
+namespace E_HandelBlazor.Services.Models;
+
+public class CustomerModel(HttpClient httpClient) : ICustomerModel
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+
+
+    public async Task<ResponseDto<SessionDto>> Auth(LoginDto model)
+    {
+        var response = await _httpClient.PostAsJsonAsync("Customer/Auth", model);
+        var result = await response.Content.ReadFromJsonAsync<ResponseDto<SessionDto>>();
+        return result!;
+    }
+    public Task<ResponseDto<CustomerDto>> Create(CustomerDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto<bool>> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto<CustomerDto>> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto<List<CustomerDto>>> List(string rol, string search)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto<bool>> Update(CustomerDto model)
+    {
+        throw new NotImplementedException();
+    }
+}

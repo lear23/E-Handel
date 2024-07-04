c2f23b818b9e74e6b308a2ecec91d298e4924e9c
lear23 <130910243+lear23@users.noreply.github.com>
2024-07-04 16:57:33 +0200

    Fixade alla views

--- E-HandelBlazor/E-HandelBlazor.Client/Program.cs
+++ E-HandelBlazor/E-HandelBlazor.Client/Program.cs
@@ -1,11 +1,18 @@
 using Blazored.LocalStorage;
 using Blazored.Toast;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5126/api/") });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredToast();
 
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
+
